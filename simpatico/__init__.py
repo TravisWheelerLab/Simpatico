1685381087f69dd5a4e7397642d215f7c6562213
Jeremiah Gaiser <jgaiser@arizona.edu>
2025-06-08 10:12:35 -0700

    fix circular import when loading config

--- simpatico/__init__.py
+++ simpatico/__init__.py
@@ -3,7 +3,6 @@ import os
 from .models.molecule_encoder.MolEncoder import MolEncoder
 from .models.protein_encoder.ProteinEncoder import ProteinEncoder
 import importlib.resources as pkg_resources
-from . import data  # assuming config.json is in simpatico/data/
 
-with pkg_resources.open_text(data, "config.json") as f:
+with pkg_resources.open_text("simpatico.data", "config.json") as f:
     config = json.load(f)
